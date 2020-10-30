1ea5900ff019c5bbbad25ba3b6ea1edcd09b41c8
l <codn3@outlook.com>
2020-10-30 11:49:07 +0000

    Styling, answers, total correct answers

--- quiz.js
+++ quiz.js
@@ -1,9 +1,18 @@
 function buildQuiz(questions, container) {
+    
+    // this function builds  a quiz it has 2 parameters: questions and container
     var output = [];
+
+    // var variable , it stores the output information (questions)
+
     var answers;
+
+    // var variable stores the answers for the quiz questions
 
     
     for (var i = 0; i < questions.length; i++) {
+
+  // this is a loop  that will loop the questions
 
         answers = [];
 
@@ -24,21 +33,39 @@ function buildQuiz(questions, container) {
         );
     }
     container.innerHTML = output.join('');
-
 }
+// this connects the container from the html document
 
-function showResults() {
-    alert("success")
+function showResults(questions, quiz) {
+  var answers = quiz.querySelectorAll(".answers")
+  var totalCorrect = 0;
+   
+   for (var i = 0; i < questions.length; i++) {
+    var currentQuestionNum = i;
+    var currentQuestion = questions[i];
+    var currentAnswer = currentQuestion.answer;
+    var currentChoices = currentQuestion.choices;
+    var answerContainer = answers[currentQuestionNum];
+
+    var answerElement = answerContainer.querySelector("input[name=question" + currentQuestionNum + "]:checked");
+
+    if(answerElement && answerElement.value === currentAnswer) {
+        answers[currentQuestionNum].style.color = 'green';
+        totalCorrect = totalCorrect + 1;
+    } else {
+        answers[currentQuestionNum].style.color = 'red';
+    }
+   }  
+   results.innerHTML = 'Total: ' + totalCorrect;
 }
-
+// this is where the quiz is contained
 var quiz = document.getElementById("quiz")
-
+// this is where you will press a button the submit answers
 var submit = document.getElementById("submit")
-
+// this is where  the results will be held
 var results = document.getElementById("results")
 
-
-
+// this variable myQuestions has an array which has the questions, choices and anwsers.
 var myQuestions = [{
         question: "The tourist attraction Big Ben in London is:",
         answer: 'c',
@@ -61,7 +88,7 @@ var myQuestions = [{
 
     {
         question: "What is the the capital city of England?",
-        answer: ' c',
+        answer: 'c',
         choices: {
             a: "Oxford",
             b: "Cambridge",
@@ -71,7 +98,7 @@ var myQuestions = [{
 
     {
         question: "What year did the Olypics take place in London Stratford ?",
-        answer: ' a',
+        answer: 'a',
         choices: {
             a: "2012",
             b: "2014",
@@ -81,7 +108,7 @@ var myQuestions = [{
 
     {
         question: "What City is the Bullring shopping centre in?",
-        answer: 'c ',
+        answer: 'c',
         choices: {
             a: "London",
             b: "Coventry",
@@ -91,9 +118,9 @@ var myQuestions = [{
 ];
 
 
-
+// this build quiz function is calling 2 arguments myQuestions and quiz
 buildQuiz(myQuestions, quiz) 
 
 submit.addEventListener("click", function () {
-    showResults()
+    showResults(myQuestions, quiz)
 });
